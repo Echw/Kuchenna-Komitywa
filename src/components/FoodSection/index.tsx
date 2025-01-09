8aa7e610ca3cfad86c8290b0bb3774ef377af2f5
echw <echwedczuk0@gmail.com>
2025-01-09 14:57:48 +0100

    fix icons background issue

--- src/components/FoodSection/index.tsx
+++ src/components/FoodSection/index.tsx
@@ -20,18 +20,13 @@ interface FoodData {
 
 const FoodSection = () => {
   return (
-    <SectionContainer
-      id="FoodSection"
-      backgroundColor="var(--mantine-color-mainGreen-10)"
-    >
+    <SectionContainer id="FoodSection" backgroundColor="var(--mantine-color-mainGreen-10)">
       <Title order={2} c="var(--mantine-color-mainGreen-8)">
         Jedzonka
       </Title>
 
       <Group className={styles.cards_section} pt={"xl"}>
-        {(foodData as FoodData).food.map((item, index) =>
-          foodCard(item, index),
-        )}
+        {(foodData as FoodData).food.map((item, index) => foodCard(item, index))}
       </Group>
     </SectionContainer>
   );
@@ -41,17 +36,9 @@ const foodCard = (item: FoodItem, index: number): React.ReactElement => {
   return (
     <Card key={index} radius="xl" className={styles.small_card}>
       <div className={styles.image_container}>
-        <Image
-          src={item.img}
-          alt={item.name.toLowerCase()}
-          className={styles.small_card_img}
-        />
+        <Image src={item.img} alt={item.name.toLowerCase()} className={styles.small_card_img} />
       </div>
-      <Title
-        order={3}
-        className={styles.small_card_title}
-        c="var(--mantine-color-orange-6)"
-      >
+      <Title order={3} className={styles.small_card_title} c="var(--mantine-color-orange-6)">
         {item.name}
       </Title>
       <List
@@ -60,7 +47,7 @@ const foodCard = (item: FoodItem, index: number): React.ReactElement => {
         size="sm"
         center
         icon={
-          <ThemeIcon c="var(--mantine-color-mainGreen-7)" size={24} radius="xl">
+          <ThemeIcon c="var(--mantine-color-mainGreen-7)" bg={"transparent"} size={24} radius="xl">
             <IconSeedingFilled style={{ width: rem(16), height: rem(16) }} />
           </ThemeIcon>
         }
